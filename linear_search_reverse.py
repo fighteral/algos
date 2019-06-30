0eed6f694f587c5b129e587122d2aec630989c70
Rishav Gupta <grishav@amazon.com>
2019-06-30 16:02:49 +0530

    Linear_search_reverse

---
+++ linear_search_reverse.py
@@ -0,0 +1,10 @@
+a=[1300,1,4,8,5,6,3,800,0,-21,-1]
+for x in range (1,len(a)):
+    key =a[x]
+    #print ("key{}".format(key))
+    for y in range (0,x):
+
+        if a[y]< key:
+           a[y],key=key,a[y]
+           a[x]=key
+print a
